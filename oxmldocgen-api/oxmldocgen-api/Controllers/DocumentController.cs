fca13d17fb66a0b666ba0c75e4d93f0cfdaf4b91
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-08-19 22:36:51 +0000

    initial commit

---
+++ oxmldocgen-api/oxmldocgen-api/Controllers/DocumentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace oxmldocgen_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        /// <summary>
+        /// Generates a document in openxml.
+        /// </summary>
+        /// <returns>OpenXML document</returns>
+        [HttpPost]
+        [Description("Generates a document in openxml.")]
+        public ActionResult<string> Create([FromBody] string html)
+        {
+            return "generated open xml document, placeholder.";
+        }
+    }
+}
---
+++ xmldocgen-api-tests/DocumentControllerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace xmldocgen_api_tests
+{
+    [TestClass]
+    public class DocumentControllerTests
+    {
+        [TestMethod]
+        public void can_convert_html_to_openxml_document()
+        {
+
+        }    
+    }
+}
