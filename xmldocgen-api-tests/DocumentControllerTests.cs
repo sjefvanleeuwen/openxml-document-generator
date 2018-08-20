e6a75adac2e4b374cead15ba6adc21efbd5689b4
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-08-20 10:32:57 +0000

    simple test generating a docx openxml document

--- xmldocgen-api-tests/DocumentControllerTests.cs
+++ xmldocgen-api-tests/DocumentControllerTests.cs
@@ -1,4 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using HtmlToOpenXml;
+using System.IO;
 
 namespace xmldocgen_api_tests
 {
@@ -8,7 +13,24 @@ namespace xmldocgen_api_tests
         [TestMethod]
         public void can_convert_html_to_openxml_document()
         {
-
+            var doc = File.ReadAllText(@".\data\letter.html");
+            using (MemoryStream generatedDocument = new MemoryStream())
+            {
+                using (WordprocessingDocument package = WordprocessingDocument.Create(generatedDocument, WordprocessingDocumentType.Document))
+                {
+                    MainDocumentPart mainPart = package.MainDocumentPart;
+                    if (mainPart == null)
+                    {
+                            mainPart = package.AddMainDocumentPart();
+                            new Document(new Body()).Save(mainPart);
+                    }
+                    HtmlConverter converter = new HtmlConverter(mainPart);
+                    converter.ParseHtml(doc);
+                    mainPart.Document.Save();
+                }
+                // visually test this.
+                File.WriteAllBytes(@".\test.docx", generatedDocument.ToArray());
+            }
         }    
     }
 }
