c12ac329cb72d62240cc368b80c9aaa493369aca
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-08-20 17:34:27 +0000

    putting tested code into document api controller

--- oxmldocgen-api/oxmldocgen-api/Controllers/DocumentController.cs
+++ oxmldocgen-api/oxmldocgen-api/Controllers/DocumentController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using HtmlToOpenXml;
 using Microsoft.AspNetCore.Mvc;
+using DocumentFormat.OpenXml;
 
 namespace oxmldocgen_api.Controllers
 {
@@ -17,9 +22,25 @@ namespace oxmldocgen_api.Controllers
         /// <returns>OpenXML document</returns>
         [HttpPost]
         [Description("Generates a document in openxml.")]
-        public ActionResult<string> Create([FromBody] string html)
+        public ActionResult<byte[]> Create(string html)
         {
-            return "generated open xml document, placeholder.";
+            using (MemoryStream generatedDocument = new MemoryStream())
+            {
+                using (WordprocessingDocument package = WordprocessingDocument.Create(generatedDocument, WordprocessingDocumentType.Document))
+                {
+                    MainDocumentPart mainPart = package.MainDocumentPart;
+                    if (mainPart == null)
+                    {
+                            mainPart = package.AddMainDocumentPart();
+                            new Document(new Body()).Save(mainPart);
+                    }
+                    HtmlConverter converter = new HtmlConverter(mainPart);
+                    converter.ParseHtml(html);
+                    mainPart.Document.Save();
+                }
+                return generatedDocument.ToArray();
+            }
+           
         }
     }
 }
